3f813642f60ac24421bd2357b0b6f261e5e8f420
williewu0908 <120082358+williewu0908@users.noreply.github.com>
2024-04-15 11:41:21 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,13 +1,33 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+import streamlit as st 
+import pandas as pd 
+import numpy as np 
+
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/' 
+'streamlit-demo-data/uber-raw-data-sep14.csv.gz') 
+
+@st.cache_data
+def load_data(nrows): 
+    data = pd.read_csv(DATA_URL, nrows=nrows) 
+    lowercase = lambda x: str(x).lower() 
+    data.rename(lowercase, axis='columns', inplace=True) 
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN]) 
+    return data
+
+# Create a text element and let the reader know the data is loading.
+data_load_state = st.text('Loading data...')
+# Load 10,000 rows of data into the dataframe.
+data = load_data(10000)
+# Notify the reader that the data was successfully loaded.
+data_load_state.text("Done! (using st.cache_data)")
+
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
+    
+st.subheader('Number of pickups by hour')
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
+
+st.subheader('Map of all pickups') 
+st.map(data)
